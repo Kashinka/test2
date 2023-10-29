e349c99f5b0a15671af79281f6d5da28ef72dbdc
Kashinka <azalena@yandex.ru>
2023-10-29 21:03:18 +0300

    Added script

---
+++ index.js
@@ -0,0 +1,74 @@
+function checkFunction () {
+
+    const regexName = new RegExp('^[а-яА-Я]{30}|[a-zA-Z]{30}$');
+    
+    if (regexName.test(document.getElementById('name').value) === false) {
+
+        document.getElementById('result').innerHTML = (`Имя нужно вводить только полностью русскими или английскими буквами`);
+
+    }
+}
+
+function register() {
+
+    let name = document.getElementById('name').value;
+
+    let email = document.getElementById('email').value;
+
+    let topic = document.getElementById('topic').value;
+
+    let message = document.getElementById('message').value;
+
+    let textarea = document.getElementById('textarea').value;
+
+    if (name === '') {
+
+        document.getElementById('result').innerHTML = (`Ошибка!`);
+
+    } else {
+
+        document.getElementById('result').innerHTML = (`Добро пожаловать, ${name}`);
+
+    }
+
+    if (email === '') {
+
+        document.getElementById('result').innerHTML = (`Ошибка!`);
+
+    } else {
+
+        document.getElementById('result').innerHTML = (`Добро пожаловать, ${name}`);
+
+    }
+
+    if (topic === '') {
+
+        document.getElementById('result').innerHTML = (`Ошибка!`);
+
+    } else {
+
+        document.getElementById('result').innerHTML = (`Добро пожаловать, ${name}`);
+
+    }
+
+    if (message === '') {
+
+        document.getElementById('result').innerHTML = (`Ошибка!`);
+
+    } else {
+
+        document.getElementById('result').innerHTML = (`Добро пожаловать, ${name}`);
+
+    }
+
+    if (textarea === '') {
+
+        document.getElementById('result').innerHTML = (`Ошибка!`);
+
+    } else {
+
+        document.getElementById('result').innerHTML = (`Добро пожаловать, ${name}!`);
+
+    }
+
+} 
